2ef524a17d7b8dc03153eaa96373f34102315fc9
kPherox <admin@mail.kr-kp.com>
2019-02-24 03:58:39 +0900

    Add rtmp protocol enum

---
+++ Sources/RtmpProtocol.swift
@@ -0,0 +1,24 @@
+//
+//  RtmpProtocol.swift
+//  SwiftRtmp
+//
+//  Created by kPherox on 2019/02/24.
+//  Copyright © 2019 kPherox. All rights reserved.
+//
+
+import Foundation
+
+public enum RtmpProtocol: Int {
+    case UNDEFINED = -1
+    case RTMP      = 0
+    case RTMPE     = 2
+    case RTMPT     = 1
+    case RTMPS     = 4
+    case RTMPTE    = 3
+    case RTMPTS    = 5
+    case RTMFP     = 8
+
+    func toInt32() -> Int32 {
+        return Int32(self.rawValue)
+    }
+}
