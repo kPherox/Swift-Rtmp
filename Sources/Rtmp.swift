dda1e64bb1c161bac1c2bee7499bd345fe0d1d58
kPherox <admin@mail.kr-kp.com>
2019-02-24 03:59:54 +0900

    Replace inout params to tuple return value

--- Sources/Rtmp.swift
+++ Sources/Rtmp.swift
@@ -22,20 +22,15 @@ public final class Rtmp {
         self.init(RTMP_Alloc())
     }
 
-    public func parseUrl(_ url: String, urlProtocol: inout Int32, host: inout String, port: inout UInt, playpath: inout String, appName: inout String) -> Bool {
+    public func parseUrl(_ url: String) -> (result: Bool, proto: RtmpProtocol, host: String, port: Int, playpath: String, appName: String) {
         var parsedHost: AVal = AVal()
         var parsedPlaypath: AVal = AVal()
         var parsedApp: AVal = AVal()
-        var parsedProtocol: Int32 = CRtmp.RTMP_PROTOCOL_UNDEFINED
+        var parsedProtocol: Int32 = RtmpProtocol.UNDEFINED.toInt32()
         var parsedPort: UInt32 = 0
         let result: Int32 = RTMP_ParseURL(url.toCString(), &parsedProtocol, &parsedHost, &parsedPort, &parsedPlaypath, &parsedApp)
 
-        urlProtocol = parsedProtocol
-        host = String(cString: parsedHost.av_val)
-        port = UInt(parsedPort)
-        playpath = String(cString: parsedPlaypath.av_val)
-        appName = String(cString: parsedApp.av_val)
-        return result != 0
+        return (result != 0, RtmpProtocol(rawValue: Int(parsedProtocol)) ?? RtmpProtocol.UNDEFINED, String(cString: parsedHost.av_val), Int(parsedPort), String(cString: parsedPlaypath.av_val), String(cString: parsedApp.av_val))
     }
 
     public func setBuffer(milliSeconds size: Int32) {
